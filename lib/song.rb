495f6cf95704f6855695e976888a4da8eaf51f36
Remmy Clay <remmy.lauren@gmail.com>
2017-12-01 00:52:54 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -36,4 +36,9 @@ def self.find_or_create_by_name(name)
   self.find_by_name(name) ? self.find_by_name(name) : self.create_by_name(name)
 end
 
+def self.alphabetical
+  self.all.sort_by{|a, b| a <=> b}
+end
+
+
 end
