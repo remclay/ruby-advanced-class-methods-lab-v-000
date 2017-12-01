8c2cd2f7d7d9d862c1a37f21fd78af571e1fc116
Remmy Clay <remmy.lauren@gmail.com>
2017-12-01 01:45:18 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -24,30 +24,30 @@ class Song
     song
   end
 
-def self.create_by_name(name)
-  song = self.new_by_name(name)
-  song.save
-  song
-end
+  def self.create_by_name(name)
+    song = self.new_by_name(name)
+    song.save
+    song
+  end
 
-def self.find_by_name(name)
-  self.all.detect{|song| song.name == name}
-end
+  def self.find_by_name(name)
+    self.all.detect{|song| song.name == name}
+  end
 
-def self.find_or_create_by_name(name)
-  self.find_by_name(name) ? self.find_by_name(name) : self.create_by_name(name)
-end
+  def self.find_or_create_by_name(name)
+    self.find_by_name(name) ? self.find_by_name(name) : self.create_by_name(name)
+  end
 
-def self.alphabetical
-  self.all.sort_by{|song| song.name}
-end
+  def self.alphabetical
+    self.all.sort_by{|song| song.name}
+  end
 
-def self.new_from_filename(file_name)
-  info = file_name.split(" - ")
-  song = self.new
-  song.artist_name = info[0]
-  song.name = (info[1].split("."))[0]
-  song
-end
+  def self.new_from_filename(file_name)
+    info = file_name.split(" - ")
+    song = self.new
+    song.artist_name = info[0]
+    song.name = (info[1].split("."))[0]
+    song
+  end
 
 end
